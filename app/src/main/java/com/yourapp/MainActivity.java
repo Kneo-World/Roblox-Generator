fdcd4d564804e5080abf2fc9a9faeee19da1ed1c
Kneo-World <pelukhnoillia@gmail.com>
2026-04-24 14:02:04 +0300

    Add MainActivity with WebView and JavaScript interface

---
+++ app/src/main/java/com/yourapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.yourapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webview);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    public class WebAppInterface {
+        Activity mActivity;
+        WebAppInterface(Activity activity) { mActivity = activity; }
+        @JavascriptInterface
+        public void finish() {
+            mActivity.finish();
+        }
+    }
+  }
